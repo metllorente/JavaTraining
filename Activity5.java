5c82424ca47bc5c8b3943705c23527fa903e615e
Marion Ericz T. Llorente <metllorente@headoffice.corp.bpi.com.ph>
2025-12-02 16:10:42 +0800

    sample commit

---
+++ Activity5.java
@@ -0,0 +1,47 @@
+package ph.com.bpi.javatraining.main;
+
+import java.util.Scanner;
+
+public class Activity5 {
+	
+	public static void main(String[] args) {
+		//Scanner input = new Scanner(System.in);
+		
+		//for (int i = 0; i <=10; i++) {
+		//	System.out.println(i);
+		//}
+		
+		//int j = 1;
+		
+		//while (j <= 10) {
+		//	System.out.println(j);
+		//	j++;
+		//}
+		
+		//int k = 0;
+			
+		//do {
+		//	System.out.println(k);
+		//	k++;
+		//} while (k <=10);
+		
+		//int[] numArray = {0,1,2,3,4,5,6,7,8,9,10};
+		//
+		//for (int number : numArray) {
+		//	System.out.println(number);
+		//}
+		
+	    int sum = 0;
+	    int i = 1;
+
+	    while (i <= 50) {
+	    	sum += i;
+	    	i++;
+	    }
+    	System.out.println("Sum is " + sum);
+
+		//input.close();
+
+	}
+
+}

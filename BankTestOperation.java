202cb48c66ef3d1ad56ed98dcb20463f3c68fe2a
Marion Ericz T. Llorente <metllorente@headoffice.corp.bpi.com.ph>
2026-01-14 19:32:57 +0800

    Upload M4_Activity6

---
+++ BankTestOperation.java
@@ -0,0 +1,6 @@
+package m4_Activity6;
+
+@FunctionalInterface
+public interface BankTestOperation {
+    void execute() throws InvalidAmountException, InsufficientFundsException;
+}
---
+++ InvalidAmountException.java
@@ -0,0 +1,7 @@
+package m4_Activity6;
+
+public class InvalidAmountException extends Exception {
+    public InvalidAmountException(String message) {
+        super(message);
+    }
+}
